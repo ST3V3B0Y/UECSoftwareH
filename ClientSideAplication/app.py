2e10d76d478791817def16c66ea04b79f30f63f6
steve <st3v3njaramillo@gmail.com>
2025-01-28 09:43:44 -0500

    Update: Ventana de confirmación Liberar equipos

--- ClientSideAplication/app.py
+++ ClientSideAplication/app.py
@@ -2,63 +2,68 @@ import socket
 import json
 import psutil
 import ctypes
+import os
 
 #SERVER_HOST = '10.100.103.142'  # Dirección IP del servidor
 SERVER_HOST = '10.33.0.93'
-SERVER_PORT = 5001
+SERVER_PORT = 5040
 #SERVER_PORT = 5040
 
 import tkinter as tk
-#from PIL import Image, ImageTk  # Pillow para manejar imágenes
-#from pynput import mouse, keyboard
+from PIL import Image, ImageTk  # Pillow para manejar imágenes
+import pynput.keyboard
+import pynput.mouse
 import threading
 
+def on_move(x, y):
+        return False
+    
+def on_click(x, y, button, pressed):
+    return False
+
+def on_press(key):
+    return False
 
 
-def bloquear_mouse():
-    with mouse.Listener(on_move=lambda x, y: False, on_click=lambda x, y, button, pressed: False) as listener:
-        listener.join()
+# Función para bloquear pantalla, mouse y teclado
+def bloquear_entrada():
 
-# Función para bloquear el teclado
-def bloquear_teclado():
-    with keyboard.Listener(on_press=lambda key: False) as listener:
-        listener.join()
 
-# Función para mostrar el protector de pantalla con imagen
-def mostrar_protector():
+    with pynput.mouse.Listener(on_move=on_move, on_click=on_click) as listener:
+        listener.start()
+
+    with pynput.keyboard.Listener(on_press=on_press) as listener:
+        listener.start()
+
     ventana = tk.Tk()
     ventana.attributes('-fullscreen', True)  # Pantalla completa
-
     # Cargar la imagen de fondo
-    imagen = Image.open("src/images/tu_imagen.jpg")  # Cambia esto por la ruta de tu imagen
+    """imagen = Image.open("src/images/tu_imagen.jpg")"""  # Cambia esto por la ruta de tu imagen
+    imagen = Image.open("C:\UECSoftware\UEC-Flask\ClientSideAplication\src\images\AIO Salones 1920 x 1080.jpg")
     imagen = imagen.resize((ventana.winfo_screenwidth(), ventana.winfo_screenheight()), Image.ANTIALIAS)  # Ajustar la imagen a la pantalla
     imagen_fondo = ImageTk.PhotoImage(imagen)
-
     # Crear un label que contenga la imagen
     label_fondo = tk.Label(ventana, image=imagen_fondo)
     label_fondo.place(x=0, y=0, relwidth=1, relheight=1)  # Cubrir toda la ventana
-
     # Evitar que se cierre con Alt+F4
     ventana.protocol("WM_DELETE_WINDOW", lambda: None)
-
     ventana.mainloop()
 
-# Función para desbloquear
-def desbloquear():
-    global listener
-    listener.stop()
+# Función para desbloquear pantalla, mouse y teclado
+def desbloquear_entrada():
+    with pynput.mouse.Listener(on_move=on_move, on_click=on_click) as listener:
+        listener.stop()
+
+    with pynput.keyboard.Listener(on_press=on_press) as listener:
+        listener.stop()
 
 # Ejecutar el bloqueo en paralelo
 def ejecutar_bloqueo():
     # Bloqueamos el mouse y teclado en hilos separados
-    mouse_hilo = threading.Thread(target=bloquear_mouse)
-    teclado_hilo = threading.Thread(target=bloquear_teclado)
+    bloqueo_hilo = threading.Thread(target=bloquear_entrada)
+ 
+    bloqueo_hilo.start()
 
-    mouse_hilo.start()
-    teclado_hilo.start()
-
-    # Mostrar el protector de pantalla
-    mostrar_protector()
 
 
 # Iniciar el bloqueo
@@ -85,7 +90,7 @@ def connect_to_server():
                 
                 command = json.loads(data)
                 if command['action'] == 'unlock':
-                    desbloqueo = desbloquear()
+                    desbloqueo = desbloquear_entrada()
                     response=json.dumps({'action':  'unlock', 'data': desbloqueo})
 
                 elif command['action'] == 'lock':
